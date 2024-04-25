b748f1dcc89fc304bab6048c1b103cd8e5b77709
Constance-wy <134715242+Constance-wy@users.noreply.github.com>
2024-04-25 16:20:17 +0800

    Create Mleval.r

---
+++ codes/Mleval.r
@@ -0,0 +1,11 @@
+library(MLeval)
+
+# Illustrative example 1 with no ties
+score <- c('P', 'N', 'P', 'N', 'N')
+label <- c(0.9, 0.8, 0.7, 0.6, 0.5)
+
+preds <- data.frame(N=c(1-score),P=score,obs=label,Group=rep('test',length(label)))
+
+# Compute AUPRC and construct PRC
+test2 <- evalm(preds,plots='r',rlinethick=0.8,fsize=8,bins=8)
+test2$proc
